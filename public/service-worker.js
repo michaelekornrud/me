b9125c3727b44522413f5fc746c862fc3dd56c50
Michael Ekornrud <ole-michael.ekornrud@ainsite.no>
2025-09-24 08:40:50 +0200

    Updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 // Basic service worker for PWA offline support
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('mekornrud-cache-v1').then(cache => {
+    caches.open('mekornrud-cache-v2').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
@@ -16,7 +16,26 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(err => {
+        console.error('Fetch failed; returning offline page instead.', err);
+        return caches.match('/index.html');
+      });
     })
   );
 });
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
