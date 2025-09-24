824c816010861f4b32589e3563373deba56040cf
Michael Ekornrud <ole-michael.ekornrud@ainsite.no>
2025-09-24 09:34:07 +0200

    Updated the serviceWorker substantially

--- src/index.js
+++ src/index.js
@@ -18,7 +18,32 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    // Force update when new service worker is available
+    const waitingServiceWorker = registration.waiting;
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener('statechange', (event) => {
+        if (event.target.state === 'activated') {
+          window.location.reload();
+        }
+      });
+      waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+    }
+  },
+  onSuccess: (registration) => {
+    console.log('SW registered: ', registration);
+  }
+});
+
+// Add aggressive cache clearing for existing users
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.update();
+    }
+  });
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
